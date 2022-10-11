cd2928867378066b4f8f122186ca2013fbfceb04
neu-szy <neu_szy@outlook.com>
2022-10-11 12:57:45 +0800

    update

---
+++ basicsr/archs/ircnn_arch.py
@@ -0,0 +1,65 @@
+from torch import nn
+from basicsr.utils.registry import ARCH_REGISTRY
+from collections import OrderedDict
+
+def sequential(*args):
+    """Advanced nn.Sequential.
+    Args:
+        nn.Sequential, nn.Module
+    Returns:
+        nn.Sequential
+    """
+    if len(args) == 1:
+        if isinstance(args[0], OrderedDict):
+            raise NotImplementedError('sequential does not support OrderedDict input.')
+        return args[0]  # No sequential is needed.
+    modules = []
+    for module in args:
+        if isinstance(module, nn.Sequential):
+            for submodule in module.children():
+                modules.append(submodule)
+        elif isinstance(module, nn.Module):
+            modules.append(module)
+    return nn.Sequential(*modules)
+
+@ARCH_REGISTRY.register()
+class IRCNN(nn.Module):
+    def __init__(self, in_nc=1, out_nc=1, nc=64):
+        """
+        # ------------------------------------
+        denoiser of IRCNN
+        in_nc: channel number of input
+        out_nc: channel number of output
+        nc: channel number
+        nb: total number of conv layers
+        act_mode: batch norm + activation function; 'BR' means BN+ReLU.
+        # ------------------------------------
+        Batch normalization and residual learning are
+        beneficial to Gaussian denoising (especially
+        for a single noise level).
+        The residual of a noisy image corrupted by additive white
+        Gaussian noise (AWGN) follows a constant
+        Gaussian distribution which stablizes batch
+        normalization during training.
+        # ------------------------------------
+        """
+        super(IRCNN, self).__init__()
+        L =[]
+        L.append(nn.Conv2d(in_channels=in_nc, out_channels=nc, kernel_size=3, stride=1, padding=1, dilation=1, bias=True))
+        L.append(nn.ReLU(inplace=True))
+        L.append(nn.Conv2d(in_channels=nc, out_channels=nc, kernel_size=3, stride=1, padding=2, dilation=2, bias=True))
+        L.append(nn.ReLU(inplace=True))
+        L.append(nn.Conv2d(in_channels=nc, out_channels=nc, kernel_size=3, stride=1, padding=3, dilation=3, bias=True))
+        L.append(nn.ReLU(inplace=True))
+        L.append(nn.Conv2d(in_channels=nc, out_channels=nc, kernel_size=3, stride=1, padding=4, dilation=4, bias=True))
+        L.append(nn.ReLU(inplace=True))
+        L.append(nn.Conv2d(in_channels=nc, out_channels=nc, kernel_size=3, stride=1, padding=3, dilation=3, bias=True))
+        L.append(nn.ReLU(inplace=True))
+        L.append(nn.Conv2d(in_channels=nc, out_channels=nc, kernel_size=3, stride=1, padding=2, dilation=2, bias=True))
+        L.append(nn.ReLU(inplace=True))
+        L.append(nn.Conv2d(in_channels=nc, out_channels=out_nc, kernel_size=3, stride=1, padding=1, dilation=1, bias=True))
+        self.model = sequential(*L)
+
+    def forward(self, x):
+        n = self.model(x)
+        return x-n
